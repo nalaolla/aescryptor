5677659a70fa819f4c38353e7713dc9eeb4cdf4f
Daniel,Kim <djkim99@lotte.net>
2020-04-10 18:04:13 +0900

    Initial commit

---
+++ src/main/java/com/nalaolla/aescryptor/AescryptorApplication.java
@@ -0,0 +1,13 @@
+package com.nalaolla.aescryptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AescryptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AescryptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nalaolla/aescryptor/AescryptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalaolla.aescryptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AescryptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
